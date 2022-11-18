668b7fd4c041e85655412c75ce7a84bf3a7f22fc
Aleksey <aleksey18rus@inbox.ru>
2022-11-18 20:23:23 +0300

    bot separate from Main

--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -1,57 +1,13 @@
-﻿using System;
-using System.Threading;
-using System.Threading.Tasks;
-using Telegram.Bot;
-using Telegram.Bot.Extensions.Polling;
-using Telegram.Bot.Types;
-using ReceiverOptions = Telegram.Bot.Extensions.Polling.ReceiverOptions;
+﻿using StartUp;
 
 namespace TelegramBotExperiments
 {
-
     class Program
     {
-        static ITelegramBotClient bot = new TelegramBotClient("5841458717:AAGygPGReGjD8T4EOq8BaMcRuYdQUx0kqqg");
-        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        static void Main()
         {
-            // Некоторые действия
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
-            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
-            {
-                var message = update.Message;
-                if (message.Text.ToLower() == "/start")
-                {
-                    await botClient.SendTextMessageAsync(message.Chat, "Добро пожаловать на борт, добрый путник!");
-                    return;
-                }
-                await botClient.SendTextMessageAsync(message.Chat, "Привет-привет!!");
-            }
-        }
-
-        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
-        {
-            // Некоторые действия
-            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
-        }
-
-
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result.FirstName);
-
-            var cts = new CancellationTokenSource();
-            var cancellationToken = cts.Token;
-            var receiverOptions = new ReceiverOptions
-            {
-                AllowedUpdates = { }, // receive all update types
-            };
-            bot.StartReceiving(
-                HandleUpdateAsync,
-                HandleErrorAsync,
-                receiverOptions,
-                cancellationToken
-            );
-            Console.ReadLine();
+            var bot = new TgBot("5841458717:AAGygPGReGjD8T4EOq8BaMcRuYdQUx0kqqg");
+            bot.InfinityPolling();
         }
     }
 }
---
+++ StartUp/TgBot.cs
@@ -0,0 +1,48 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+
+namespace StartUp
+{
+    public class TgBot
+    {
+        public ITelegramBotClient Bot;
+        private string token;
+        public async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+            if (update == null) return;
+            if (update.Type != UpdateType.Message) return;
+
+            var message = update.Message;
+            if(message != null)
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Привет-привет!!");
+        }
+
+        public async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+        public void InfinityPolling()
+        { 
+            while (true)
+            {
+                var command = Console.ReadLine()?.ToLower();
+                if (command == "stop")
+                    return;
+            }
+        }
+
+        public TgBot(string token)
+        {
+            this.token = token;
+            Bot = new TelegramBotClient(this.token);
+
+            Bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync
+            );
+        }
+    }
+}
