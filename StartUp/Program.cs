10b4b1fe01bdf516ece2ef5cdd3b2ffc1a5375a6
Aleksey <aleksey18rus@inbox.ru>
2022-11-18 18:01:54 +0300

    token add

--- StartUp/Program.cs
+++ StartUp/Program.cs
@@ -11,7 +11,7 @@ namespace TelegramBotExperiments
 
     class Program
     {
-        static ITelegramBotClient bot = new TelegramBotClient("TOKEN");
+        static ITelegramBotClient bot = new TelegramBotClient("5841458717:AAGygPGReGjD8T4EOq8BaMcRuYdQUx0kqqg");
         public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             // Некоторые действия
